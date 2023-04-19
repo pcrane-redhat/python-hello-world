7e211cd1c09c78a4fa69a1441139d5b919a861c5
pcrane-redhat <114171262+pcrane-redhat@users.noreply.github.com>
2023-04-19 09:56:37 -0400

    Update app.py
    back to normal

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask
 import os
-import random
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    rand = random()
     return  "hello gain"
 
 if __name__ == '__main__':
